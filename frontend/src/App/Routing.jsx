cccd0d770e336ad89d5ca3c52f50a7ea77c4e7ef
Kin1599 <dkonopliannikov05@mail.ru>
2025-10-05 01:43:19 +0800

    fix style

--- frontend/src/App/Routing.jsx
+++ frontend/src/App/Routing.jsx
@@ -7,7 +7,7 @@ import LoginPage from '../Pages/LoginPage';
 import NotFoundPage from '../Pages/NotFoundPage';
 
 import {Navigate} from 'react-router-dom';
-import {useStore} from './store';
+import {useStore} from './Store';
 
 const ProtectedRoute = ({children}) => {
   const {user} = useStore();

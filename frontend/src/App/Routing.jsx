372d10acaafed82da667de4ac520529a3891e7f9
Kin1599 <dkonopliannikov05@mail.ru>
2025-10-05 10:27:50 +0800

    fix

--- frontend/src/App/Routing.jsx
+++ frontend/src/App/Routing.jsx
@@ -6,7 +6,7 @@ import ProfilePage from '../Pages/ProfilePage';
 import NotFoundPage from '../Pages/NotFoundPage';
 
 import {Navigate} from 'react-router-dom';
-import {useStore} from './Store';
+import {useStore} from './store';
 
 const ProtectedRoute = ({children}) => {
   const {user} = useStore();

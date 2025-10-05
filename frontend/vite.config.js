b43e40f8c2d835b6c85785a075f98a6fac2825e4
Kin1599 <dkonopliannikov05@mail.ru>
2025-10-05 10:32:29 +0800

    add maximumFileSizeToCacheInBytes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -38,6 +38,7 @@ export default defineConfig({
         ]
       },
       workbox: {
+        maximumFileSizeToCacheInBytes: 4 * 1024 * 1024,
         globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
         runtimeCaching: [
           {
